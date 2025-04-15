091e2dd2b41be7645f5b565f8ab74d6dd6f5a231
Xiao753463 <a753463@gmail.com>
2025-04-15 15:29:14 +0800

    fix: 移除fb圖

--- src/app/components/Menu/index.js
+++ src/app/components/Menu/index.js
@@ -58,7 +58,7 @@ export default function Menu() {
 
   const socialMediaLinks = [
     {
-      imgSrc: '',
+      imgSrc: '/imgs/FB-Icon.png',
       url: 'https://www.facebook.com/hsiao.chunyun/',
     },
     {
